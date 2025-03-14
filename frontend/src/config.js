2842d36957c551f2efca11784dcab862f1556c3e
Shiraz <shirazkk1@gmail.com>
2025-03-15 00:59:36 +0530

    Merged remote changes and updated local changes

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 export const BASE_URL = "https://threed-models-viewer-backend.onrender.com";
+=======
+export const BASE_URL = "http://localhost:5000";
+>>>>>>> Stashed changes
